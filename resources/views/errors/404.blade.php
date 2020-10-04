7561961f22022b26337c1493b01d17fbe8da3382
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-10-04 11:33:06 +0000

    navbar fix on 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,6 +4,11 @@
 
 @section('pageTitle', 'Page Not Found')
 
+@push('additionalMeta')
+    <!-- Bootstrap 4.5.2 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+@endpush
+
 @section('body')
 
     <div class="flex flex-col h-full justify-center items-center">
